cc481de9072081c4f2a10a50bbc888a0838c00d3
Cloud-The-Mind-Sculptor <92057118+Cloud-The-Mind-Sculptor@users.noreply.github.com>
2021-10-20 22:10:40 -0500

    Update Bradley?.rkt
    Final Version of the Code that did end up functioning

--- Bradley?.rkt
+++ Bradley?.rkt
@@ -1,12 +1,11 @@
 (define (bridgely? lon) 
     (local [(define ALPHA (first lon))
-            (define (last_val l) (cond [(empty? (rest l)) (first l)]
-                                       [else (last_val (rest l))]))
+            (define (last_val l) 
+                (cond [(empty? (rest l)) (first l)]
+                      [else (last_val (rest l))]))
             (define OMEGA (last_val lon))]
     (cond [(> 3 (length lon)) false]
-          [(empty? (rest (rest (rest lon)))) (if (and (< OMEGA (first (rest lon))) (< OMEGA (first lon))) true false)]
-          [(> ALPHA (rest lon)) false]
-          [(> OMEGA (rest lon)) false]
-          [(and (< ALPHA (first (rest lon))) (< OMEGA (first (rest lon)))) (bridgley? (rest lon))]
-          [else false]
+          [(= 3 (length lon)) (and (> (first (rest lon)) ALPHA) (> (first (rest lon)) OMEGA))]
+          [(or (>= ALPHA (first (rest lon))) (>= OMEGA (first (rest lon)))) false]
+          [else (bridgely? (cons ALPHA (rest (rest lon))))]
           )))
